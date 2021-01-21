72841643baa1dfea56a5d7e7fcdaada3bb51a664
praveen naini <npraveenreddy9@gmail.com>
2021-01-21 14:50:07 -0600

    added delete key word

--- index.js
+++ index.js
@@ -1,63 +1,121 @@
 
 var res="";
-function display1(num){
+// function display1(num){
 
     
 
-    if(num==="1"){
+//     if(num==="1"){
 
-        res=res+num;
-    }
-    else if(num==="2"){
-        res=res+num;
-    }
-    else if(num==="3"){
+//         res=res+num;
+//     }
+//     else if(num==="2"){
+//         res=res+num;
+//     }
+//     else if(num==="3"){
 
-        res=res+num;
-    }else if(num==="4"){
+//         res=res+num;
+//     }else if(num==="4"){
 
-        res=res+num;
-    }else if(num==="5"){
+//         res=res+num;
+//     }else if(num==="5"){
 
-        res=res+num;
-    }else if(num==="6"){
+//         res=res+num;
+//     }else if(num==="6"){
 
-        res=res+num;
-    }else if(num==="7"){
+//         res=res+num;
+//     }else if(num==="7"){
 
-        res=res+num;
-    }
-    else if(num==="8"){
+//         res=res+num;
+//     }
+//     else if(num==="8"){
 
-        res=res+num;
-    }else if(num==="9"){
+//         res=res+num;
+//     }else if(num==="9"){
 
-        res=res+num;
-    }
-    else if(num==="0"){
+//         res=res+num;
+//     }
+//     else if(num==="0"){
 
-        res=res+num;
-    }
-    document.getElementById("calling").innerText=res;
-}
+//         res=res+num;
+//     }
+//     document.getElementById("calling").innerText=res;
+// }
 
     
-    function call(){
+//     function call(){
 
-        if(res.length>11){
+//         if(res.length>11){
 
-            document.getElementById("calling").innerText="Invalid Number"+res;
-        }
-        else{
-            document.getElementById("calling").innerText="calling..."+res;
+//             document.getElementById("calling").innerText="Invalid Number"+res;
+//         }
+//         else{
+//             document.getElementById("calling").innerText="calling..."+res;
 
-        }
+//         }
       
-    }
+//     }
    
 
 
+function display1(str){
+    switch(str){  
+        case "1":  
+        res=res+str;  
+        break; 
+        case "1":  
+        res=res+str;  
+        break; 
+        case "2":  
+        res=res+str;  
+        break; 
+        case "3":  
+        res=res+str;  
+        break; 
+        case "4":  
+        res=res+str;  
+        break; 
+        case "5":  
+        res=res+str;  
+        break; 
+        case "6":  
+        res=res+str;  
+        break; 
+        case "7":  
+        res=res+str;  
+        break; 
+        case "8":  
+        res=res+str;  
+        break; 
+        case "9":  
+        res=res+str;  
+        break; 
+        case "0":  
+        res=res+str;  
+        break; 
+        
+      
+        
+}
+document.getElementById("calling").innerText=res;
+}
 
 
+function call(){
 
+            if(res.length>11){
+    
+                document.getElementById("calling").innerText="Invalid Number"+res;
+            }
+            else{
+                document.getElementById("calling").innerText="calling..."+res;
+    
+            }
+          
+        }
+       
+        function deleteKey(num){
+
+            res=res.substring(0,res.length-1);
+            document.getElementById("calling").innerText=res;
+        }
 
